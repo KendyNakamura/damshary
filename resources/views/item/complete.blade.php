1cd43b89ea07ba57cd3c157893bbdda8f5012cb2
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-07-31 22:13:41 +0900

    ビューの変更

---
+++ resources/views/item/complete.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.all')
+
+@section('content')
+<h1>購入完了しました！</h1>
+
+<a href="{{ route('item.index') }}">トップページ</a>へ
+@endsection
+
+@section('footer')
+	2018 Kenji Nakamura
+@endsection
